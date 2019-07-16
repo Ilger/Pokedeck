89db1aa5dc7552d8097b2f8a32ac21814195fdc0
ilger <ilgerdepraetere@gmail.com>
2019-07-16 13:44:58 +0200

    add test code to post api data in right elelments
    Co-Authored-By: Merel van puymbroeck <merelvanpuymbroeck@gmail.com>
    Co-Authored-By: JanHaerensBecode <haerens.jan@gmail.com>

--- master.js
+++ master.js
@@ -1,6 +1,13 @@
 
 
+const sprite = document.getElementsByName('sprite')[0];
+const move = document.getElementsByName('moves')[0];
+const evolution = document.getElementsByName('evolution')[0];
 
+console.log(document.getElementsByName('moves'));
+console.log(sprite, move, evolution);
+console.log(move);
+console.log(evolution);
 const lookingForPokemon = document.getElementById("pokemonChoosen");
 // document.getElementsByClassName('input');
 let response;
@@ -11,12 +18,10 @@ let response;
 lookingForPokemon.addEventListener('keypress', function (enter) {
   var key = enter.which || enter.keyCode;
   if (key === 13) { // 13 is enter
-    
+
     // code for enter
-    console.log('enter pressed');
-    apiCall();
-    console.log('response:');
-    console.log(response);
+    // apiCall();
+    postInfo();
   }
 });
 
@@ -28,20 +33,18 @@ function apiCall(params) {
 fetch('https://pokeapi.co/api/v2/pokemon/4')
   .then(function(response) {
     console.log('json');
-    console.log(response);
     return response.json();
   })
   .then(function(myJson) {
     console.log('response json');
-    
+
     console.log(JSON.stringify(myJson));
     console.log('myJson');
     console.log(myJson);
+    postInfo(myJson)
   });
 
 }
-
-
 
 
 // post api data:
@@ -49,7 +52,17 @@ fetch('https://pokeapi.co/api/v2/pokemon/4')
 // An image (sprite)
 // At least 4 "moves"
 // The previous evolution, only if it exists, along with their name and image
+function postInfo() {
 
+  sprite.innerHTML = "foto";
+  for  (let i = 0; i < move.childElementCount; i++) {
+    move.children[i].innerText = `move${(i + 1)}`;
+    console.log(move.childElementCount);
+    console.log(i);
+  }
+  evolution.innerHTML = 'first evo';
+
+}
 
 
 
@@ -62,14 +75,14 @@ fetch('https://pokeapi.co/api/v2/pokemon/4')
 // function apiCall(params) {
 //   let pokemon = lookingForPokemon.value;
 //   let apilink = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-  
+
 //   let  = new XMLHttpRequest();
 //   pokedex.addEventListener("load", () => {
 //     alert(pokedex.responseText)
 //   });
 //   pokedex.open("GET", "https://en.pokedexpedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein");
 //   pokedex.send();
-  
+
 // }
 
 
